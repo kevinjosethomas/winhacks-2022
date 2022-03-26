e09cdc294b750c058de6fc03a1bec09de803f532
Kevin Thomas <kevin.jt2007@gmail.com>
2022-03-26 08:15:56 -0700

    feat: added basic project component

--- packages/web/src/pages/projects/index.p.tsx
+++ packages/web/src/pages/projects/index.p.tsx
@@ -3,16 +3,37 @@ import { GetServerSidePropsContext } from "next";
 import { Authenticate } from "api/user";
 import Default from "ui/layouts/Default";
 import Header from "./index/components/Header";
+import Project from "./index/components/Project";
 
 type ProjectsProps = {
   user: Record<string, any>;
 };
 
+const PROJECTS = [
+  {
+    project_id: 1,
+    name: "Teaching @ Maywood Secondary School",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla, massa id convallis congue, sem felis placerat velit, a placerat justo orci nec sem. Proin ultrices ligula erat. In scelerisque tempus ante, ut tristique felis mattis in. Donec et magna nisl. Integer risus quam, sodales vitae egestas fringilla, semper vitae lectus. Cras congue odio dolor. Ut molestie dolor a augue ultricies tempor. Suspendisse luctus, lectus porttitor lacinia sodales, lorem mauris ullamcorper leo, tristique condimentum neque tortor nec ipsum. Quisque ante metus, scelerisque vel est vel, finibus egestas neque.",
+    tasks: ["Data Analytics", "Graphic Design", "Literature Review"],
+    affiliation: "St. Clair College",
+    duration: 1,
+    required_people: 10,
+    accepting: true,
+    approved: true,
+  },
+];
+
 export default function Projects(props: ProjectsProps) {
   return (
     <Default user={props.user}>
-      <div className="flex w-full flex-col">
+      <div className="flex w-full flex-col space-y-8">
         <Header user={props.user} />
+        <div className="flex w-full flex-col items-start justify-start">
+          {PROJECTS.map((project, index) => (
+            <Project key={index} {...project} />
+          ))}
+        </div>
       </div>
     </Default>
   );
--- packages/web/src/pages/projects/index/components/Header.tsx
+++ packages/web/src/pages/projects/index/components/Header.tsx
@@ -7,7 +7,7 @@ type HeaderProps = {
 function Header(props: HeaderProps): JSX.Element {
   return (
     <div className="flex w-full items-center justify-between">
-      <p className="font-monument text-5xl text-white">Open Projects</p>
+      <p className="font-monument text-5xl text-white">Project Opportunities</p>
       {(props.user.type === 2 || props.user.type === 3) && (
         <Link href="/project/create">
           <a className="flex items-center rounded-xl bg-blue-800 px-6 py-2 transition duration-300 hover:bg-blue-900">
